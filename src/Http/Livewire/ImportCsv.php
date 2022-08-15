c18884b2a596d90889e3f2fe8a9fb5605771a2eb
ousid <imo.ossama1@gmail.com>
2022-08-15 11:58:15 +0100

    add: file columns logic

--- src/Http/Livewire/ImportCsv.php
+++ src/Http/Livewire/ImportCsv.php
@@ -9,14 +9,56 @@ class ImportCsv extends Component
 {
     use WithFileUploads;
 
-    /** @var string */
+    /** @var string  $model*/
     public string $model;
 
-    /** @var string */
+    /** @var string  $file*/
     public string $file;
+
+    /** @var array $columnsToMap */
+    public array $columnsToMap = [];
+
+    /** @var array $requiredColumns */
+    public array $requiredColumns = [];
+
+    /** @var array $columnLabels */
+    public array $columnLabels = [];
+
+    public function mount()
+    {
+        $this->columnsToMap = collect($this->columnsToMap)
+                ->mapWithKeys(fn ($column): array => [$column => ''])
+                ->toArray();
+
+        if ($this->columnLabels) {
+            $this->columnLabels = collect($this->requiredColumns)
+                ->mapWithKeys(function ($column): array {
+                    return [
+                        'columnsToMap.' . $column => strtolower($this->columnLabels[$column]),
+                    ];
+                })->toArray();
+        }
+
+        $this->requiredColumns = collect($this->requiredColumns)
+            ->mapWithKeys(function ($column): array {
+                return ['columnsToMap.' . $column => 'required'];
+            })->toArray();
+    }
 
     public function render()
     {
         return view('laravel-csv::livewire.import-csv');
+    }
+
+    protected function rules()
+    {
+        return [
+            'file' => 'required|file|mimes:csv,txt',
+        ] + $this->requiredColumns;
+    }
+
+    protected function validationAttributes()
+    {
+        return $this->columnLabels;
     }
 }
--- tests/ImportCsvTest.php
+++ tests/ImportCsvTest.php
@@ -32,3 +32,82 @@ it('renders import CSV component with model and file', function () {
     ->assertSet('file', $file)
     ->assertSuccessful();
 });
+
+it('transfers columnsToMap into an associative array', function () {
+    $columnsToMap = [
+        'name',
+        'email',
+        'phone',
+    ];
+    $model = Customer::class;
+    $file = __DIR__.'./Data/customers.csv';
+
+    livewire(ImportCsv::class, [
+        'model' => $model,
+        'file' => $file,
+        'columnsToMap' => $columnsToMap,
+    ])
+    ->assertSet('model', $model)
+    ->assertSet('file', $file)
+    ->assertSet('columnsToMap', [
+        'name' => '',
+        'email' => '',
+        'phone' => '',
+    ])
+    ->assertSuccessful();
+});
+
+it('maps requiredColumns property into columnsToMap required state', function () {
+    $requiredColumns = [
+        'name',
+        'email',
+        'phone',
+    ];
+    $model = Customer::class;
+    $file = __DIR__.'./Data/customers.csv';
+
+    livewire(ImportCsv::class, [
+        'model' => $model,
+        'file' => $file,
+        'requiredColumns' => $requiredColumns,
+    ])
+    ->assertSet('model', $model)
+    ->assertSet('file', $file)
+    ->assertSet('requiredColumns', [
+        'columnsToMap.name' => 'required',
+        'columnsToMap.email' => 'required',
+        'columnsToMap.phone' => 'required',
+    ]);
+});
+
+it('maps through columnsLabels to validate attributes', function () {
+    $columnsToMap = [
+        'name', 'email', 'phone',
+    ];
+
+    $requiredColumns = [
+        'name', 'email',
+    ];
+
+    $columnLabels = [
+        'name' => 'Name',
+        'email' => 'Email',
+    ];
+
+    $model = Customer::class;
+    $file = __DIR__.'./Data/customers.csv';
+
+    livewire(ImportCsv::class, [
+        'model' => $model,
+        'file' => $file,
+        'columnsToMap' => $columnsToMap,
+        'requiredColumns' => $requiredColumns,
+        'columnLabels' => $columnLabels,
+    ])
+    ->assertSet('model', $model)
+    ->assertSet('file', $file)
+    ->assertSet('columnLabels', [
+        'columnsToMap.name' => 'name',
+        'columnsToMap.email' => 'email',
+    ]);
+});
