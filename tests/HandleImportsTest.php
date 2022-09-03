22e0e845899b10e44a9003fe21f970a346b8637d
ousid <imo.ossama1@gmail.com>
2022-09-03 11:22:14 +0100

    update imports test

--- tests/HandleImportsTest.php
+++ tests/HandleImportsTest.php
@@ -1,6 +1,8 @@
 <?php
 
 use Coderflex\LaravelCsv\Http\Livewire\HandleImports;
+use Coderflex\LaravelCsv\Tests\Models\User;
+
 use function Pest\Livewire\livewire;
 
 it('renders handle imports component', function () {
@@ -9,6 +11,8 @@ it('renders handle imports component', function () {
 });
 
 it('renders handle imports component with model', function () {
+    $this->actingAs(User::factory()->create());
+
     $model = Customer::class;
 
     livewire(HandleImports::class, [
