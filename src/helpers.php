b6dcc43e258e7ca3c47d21516754b5a554f8b8ca
ousid <ousid@users.noreply.github.com>
2022-09-11 11:55:59 +0000

    Fix styling

--- src/helpers.php
+++ src/helpers.php
@@ -5,11 +5,12 @@ namespace Coderflex\LaravelCsv;
 if (! function_exists('Coderflex\LaravelCsv\csv_view_path')) {
     /**
      * Get the evaluated view content from the livewire view
-     * 
+     *
      * @param  string|null  $view
      * @return string
      */
-    function csv_view_path($view) {
-        return 'laravel-csv::livewire.' . config('laravel_csv.layout') . '.' . $view;
+    function csv_view_path($view)
+    {
+        return 'laravel-csv::livewire.'.config('laravel_csv.layout').'.'.$view;
     }
 }
