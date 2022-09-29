b2878099359a0c1cf37d0d68b69e2d303e343ca3
ousid <ousid@users.noreply.github.com>
2022-09-29 23:00:22 +0000

    Fix styling

--- tests/CsvImporterTest.php
+++ tests/CsvImporterTest.php
@@ -23,7 +23,7 @@ it('renders import CSV component with model', function () {
 });
 
 it('renders import CSV component with model and file', function () {
-    $path = __DIR__ . '/stubs/customers.csv';
+    $path = __DIR__.'/stubs/customers.csv';
     $model = Customer::class;
 
     chmod($path, 0755);
