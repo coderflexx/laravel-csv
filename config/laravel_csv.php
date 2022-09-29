bc0384ee5cfa734a0e58c2c6942357ed642139e8
ousid <ousid@users.noreply.github.com>
2022-09-29 22:28:03 +0000

    Fix styling

--- config/laravel_csv.php
+++ config/laravel_csv.php
@@ -7,7 +7,7 @@ return [
     | Default Layout
     |--------------------------------------------------------------------------
     |
-    | This package plans on supporting multiple CSS frameworks. 
+    | This package plans on supporting multiple CSS frameworks.
     | Currently, 'tailwindcss' is the default and only supported framework.
     |
     */
@@ -19,7 +19,7 @@ return [
     |--------------------------------------------------------------------------
     |
     | The default maximumum file size that can be imported by this
-    | package is 20MB. If you wish to increase/decrease this value, 
+    | package is 20MB. If you wish to increase/decrease this value,
     | change the value in KB below.
     |
     */
