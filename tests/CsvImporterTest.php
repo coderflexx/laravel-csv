b9865abe77933bb140e8a671c944ad40c246c3d7
ousid <imo.ossama1@gmail.com>
2022-09-30 00:10:35 +0100

    wip

--- tests/CsvImporterTest.php
+++ tests/CsvImporterTest.php
@@ -23,6 +23,8 @@ it('renders import CSV component with model', function () {
 });
 
 it('renders import CSV component with model and file', function () {
+    Storage::fake('documents');
+    
     $path = __DIR__ . '/stubs/customers.csv';
     $model = Customer::class;
 
